5d7c01f0b5bc78e1648fe9047f2771c65f0877a8
Cyber OS Lab <99838653+cyberoslab@users.noreply.github.com>
2023-06-27 08:37:11 -0700

    Create checksum.ts

---
+++ src/checksum.ts
@@ -0,0 +1,22 @@
+import * as core from "@actions/core";
+import * as crypto from "crypto";
+import * as fs from "fs";
+
+export function verifyChecksum(downloadPath: string) {
+  const fileBuffer: Buffer = fs.readFileSync(downloadPath);
+  const checksum: string = crypto
+    .createHash("sha256")
+    .update(fileBuffer)
+    .digest("hex"); // checksum of downloaded file
+
+  const expectedChecksum: string =
+    "79cc2df62f6eba9ab4ceadbbdfca4d20ef5b14e1439a98eaa559142b8dd61aac"; // checksum for v0.13.4
+
+  if (checksum !== expectedChecksum) {
+    core.setFailed(
+      `Checksum verification failed, expected ${expectedChecksum} instead got ${checksum}`
+    );
+  }
+
+  core.debug("Checksum verification passed.");
+}
